ae04105dc6827307602647e68e4b8deff1b9894b
Michael Frieze <michaelfrieze@icloud.com>
2023-07-13 14:20:39 -0400

    Using a parallel route to intercept the sign-in page

---
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+      <h1>INTERCEPT</h1>
+    </div>
+  );
+};
+
+export default page;
