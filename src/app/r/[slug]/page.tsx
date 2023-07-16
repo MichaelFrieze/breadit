7f44d03f63e8b36658283d12035119f15efee5fc
Michael Frieze <michaelfrieze@icloud.com>
2023-07-16 13:11:05 -0400

    created SubredditPage

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config';
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { notFound } from 'next/navigation';
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const SubredditPage = async ({ params }: PageProps) => {
+  const { slug } = params;
+
+  const session = await getAuthSession();
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  if (!subreddit) return notFound();
+
+  return (
+    <div>
+      <h1>page</h1>
+    </div>
+  );
+};
+
+export default SubredditPage;
