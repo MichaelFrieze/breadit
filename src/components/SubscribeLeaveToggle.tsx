78059161da4724df9e16263823b29e24603d51fe
Michael Frieze <michaelfrieze@icloud.com>
2023-07-19 21:28:18 -0400

    Working on SubscribeLeaveToggle

--- src/components/SubscribeLeaveToggle.tsx
+++ src/components/SubscribeLeaveToggle.tsx
@@ -2,11 +2,29 @@
 
 import { FC } from 'react';
 import { Button } from './ui/Button';
+import { useMutation } from '@tanstack/react-query';
+import { SubscribeToSubredditPayload } from '@/lib/validators/subreddit';
+import axios from 'axios';
 
-interface SubscribeLeaveToggleProps {}
+interface SubscribeLeaveToggleProps {
+  subredditId: string;
+}
 
-const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({}) => {
+const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({
+  subredditId,
+}) => {
   const isSubscribed = false;
+
+  const {} = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubredditPayload = {
+        subredditId,
+      };
+
+      const { data } = await axios.post('/api/subreddit/subscribe', payload);
+      return data as string;
+    },
+  });
 
   return isSubscribed ? (
     <Button className="w-full mt-1 mb-4">Leave community</Button>
