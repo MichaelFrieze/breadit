f3526b4e6597a538bf20e71d8f35afa8b739e318
Michael Frieze <michaelfrieze@icloud.com>
2023-08-25 18:51:17 -0400

    Working on the Editor component using EditorJS

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,66 @@
 'use client';
 
-import { FC } from 'react';
+import { PostCreationRequest, PostValidator } from '@/lib/validators/post';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FC, useCallback, useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import TextareaAutosize from 'react-textarea-autosize';
+import type EditorJS from '@editorjs/editorjs';
 
-interface EditorProps {}
+interface EditorProps {
+  subredditId: string;
+}
 
-const Editor: FC<EditorProps> = ({}) => {
+const Editor: FC<EditorProps> = ({ subredditId }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subredditId,
+      title: '',
+      content: null,
+    },
+  });
+
+  const ref = useRef<EditorJS>();
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import('@editorjs/editorjs')).default;
+    const Header = (await import('@editorjs/header')).default;
+    const Embed = (await import('@editorjs/embed')).default;
+    const Table = (await import('@editorjs/table')).default;
+    const List = (await import('@editorjs/list')).default;
+    const Code = (await import('@editorjs/code')).default;
+    const LinkTool = (await import('@editorjs/link')).default;
+    const InlineCode = (await import('@editorjs/inline-code')).default;
+    const ImageTool = (await import('@editorjs/image')).default;
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          ref.current = editor;
+        },
+        placeholder: 'Type here to write your post...',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: '/api/link',
+            },
+          },
+          image: {},
+        },
+      });
+    }
+  }, []);
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
