6f0a239edd7ca5609cedbac339a6689c66fba605
Michael Frieze <michaelfrieze@icloud.com>
2023-09-22 23:32:35 -0400

    Added the sSearchBar back and adjusted NavBar gap and sign-in button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions);
   return (
     <div className="fixed top-0 inset-x-0 h-19.5 bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-6">
         {/* logo */}
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
@@ -20,13 +20,18 @@ const Navbar = async () => {
         </Link>
 
         {/* search bar */}
-        {/* <SearchBar /> */}
+        <SearchBar />
 
         {/* actions */}
         {session?.user ? (
           <UserAccountNav user={session.user} />
         ) : (
-          <Link href="/sign-in" className={buttonVariants()}>
+          <Link
+            href="/sign-in"
+            className={buttonVariants({
+              className: 'whitespace-nowrap',
+            })}
+          >
             Sign In
           </Link>
         )}
